6a4151889edad81bcdd060d9d2f14b9fb05f5353
waron97 <winkler.aron5gmail.com>
2023-01-05 16:47:25 +0100

    fix bug where reference was incorrectly taken

--- src/metrics/Metric.py
+++ src/metrics/Metric.py
@@ -10,6 +10,7 @@ class Result:
     sent_id: str
     lang_code: str
     value: float
+    sent_id_eng: str = None
     reference_value: float = None
     offset_from_reference: float = None
 
@@ -37,6 +38,7 @@ class Metric(ABC):
             results.append(Result(
                 lang_code=treebank.lang_code,
                 sent_id=sentence.sent_id,
+                sent_id_eng=sentence.sent_id_eng,
                 value=value
             ))
         return results
@@ -70,7 +72,7 @@ class Metric(ABC):
             treebank_values = [result.value for result in treebank_result]
             if reference_values:
                 for result in treebank_result:
-                    result.reference_value = reference_values[result.sent_id]
+                    result.reference_value = reference_values[result.sent_id_eng]
                     result.offset_from_reference = result.value - \
                         result.reference_value
             final.treebanks[treebank.lang_code] = treebank_result
