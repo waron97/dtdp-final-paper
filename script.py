ca41070f5692bf19e4f8b86ddd5a50c355d264b0
waron97 <winkler.aron5gmail.com>
2023-01-07 11:11:04 +0100

    update main script

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import os
 
 def main():
     eng_path = os.path.join(os.path.dirname(__file__), 'corpus/en.conllu')
-    eng = Treebank.from_file(eng_path, 'en')
+    eng = Treebank.from_file(eng_path, 'eng')
     deu_path = os.path.join(os.path.dirname(__file__), 'corpus/deu.conllu')
     deu = Treebank.from_file(deu_path, 'deu')
     hun_path = os.path.join(os.path.dirname(__file__), 'corpus/hun.conllu')
@@ -14,9 +14,11 @@ def main():
         os.path.dirname(__file__), 'corpus/madarin.conllu')
     mandarin = Treebank.from_file(mandarin_path, 'cmn')
 
+    # token count
     tokenCount = TokenCount(include_punct=False)
     tokenCountResult = tokenCount.for_parellel_treebanks(
         [deu, hun, mandarin], reference_treebank=eng)
+    print(tokenCountResult.to_pandas())
 
 
 if __name__ == '__main__':
