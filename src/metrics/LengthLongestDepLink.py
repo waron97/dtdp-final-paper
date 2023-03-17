883d3279f448d0e2b7751f477df70447b8f5cf7f
waron97 <winkler.aron5gmail.com>
2023-03-17 15:39:05 +0100

    implement metric

--- src/metrics/LengthLongestDepLink.py
+++ src/metrics/LengthLongestDepLink.py
@@ -4,4 +4,11 @@ from ..boxes.Sentence import Sentence
 
 class LengthLongestDepLink(Metric):
     def for_sentence(self, sentence: Sentence) -> float:
-        return super().for_sentence(sentence)
+        longest_so_far = 0
+        for word in sentence.rows:
+            head = word.head_index
+            index = word.index
+            distance = abs(int(head) - int(index))
+            if distance > longest_so_far:
+                longest_so_far = distance
+        return longest_so_far
